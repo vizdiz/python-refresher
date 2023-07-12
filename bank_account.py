1c67cfa6b70e6ceb6d08f9dc5943df4459786ea3
vismay <vismayravikumar@gmail.com>
2023-07-12 14:11:41 -0400

    Added simulation Jupyer Notebook, wrote code to simulate an object in water

--- bank_account.py
+++ bank_account.py
@@ -1,16 +1,21 @@
 class BankAccount:
 
-    def __init__(self, balance, name, account_number):
+    def __init__(self, balance: float or int, name: str, account_number: int):
         self.balance = balance
         self.name = name
         self.account_number = account_number
 
-    def withdraw(self, withdrawal):
-        if (self.balance >= withdrawal):
+    def withdraw(self, withdrawal: int or float):
+        if self.balance >= withdrawal:
             self.balance -= withdrawal
+        else:
+            raise ValueError("Insufficient Funds")
     
-    def deposit(self, deposit):
-        self.balance += deposit
+    def deposit(self, deposit: int or float):
+        if deposit > 0:
+            self.balance += deposit
+        else:
+            raise ValueError("Cannot deposit debt into the account")
 
     def print_balance(self):
         print(self.balance)
